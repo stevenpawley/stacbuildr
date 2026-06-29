ddd1544ac852116f4923aa28392d408faf917b4f
Steven Pawley <dr.stevenpawley@gmail.com>
2026-06-29 13:35:06 -0600

    u

--- R/integrations.R
+++ R/integrations.R
@@ -407,8 +407,8 @@ add_projection_metadata_terra <- function(item, terra_obj) {
     0,                          # row rotation
     terra::ext(terra_obj)$xmin, # top-left x
     0,                          # column rotation
-    -terra::yres(terra_obj),     # pixel height (negative for north up)
-    terra::ext(terra_obj)$ymax, # top-left-y
+    -terra::yres(terra_obj),    # pixel height (negative for north up)
+    terra::ext(terra_obj)$ymax  # top-left-y
   )
 
   item
